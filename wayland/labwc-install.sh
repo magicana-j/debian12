c8ef58b3619ef8d27a9e7741add0b0bd9c6d8889
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-01 11:22:34 +0900

    labwc-install.sh を更新

--- wayland/labwc-install.sh
+++ wayland/labwc-install.sh
@@ -6,7 +6,11 @@ sudo apt install labwc
 DSTDIR=~/.config/labwc
 if [ ! -d $DSTDIR ]; then
   mkdir $DSTDIR
-#  cp /usr/local/etc/sway/config $DSTDIR+=/
+  wget https://raw.githubusercontent.com/labwc/labwc/master/docs/environment -O ~/.config/labwc/environment
+  wget https://raw.githubusercontent.com/labwc/labwc/master/docs/autostart -O ~/.config/labwc/autostart
+  wget https://raw.githubusercontent.com/labwc/labwc/master/docs/menu.xml -O ~/.config/labwc/menu.xml
+  wget https://raw.githubusercontent.com/labwc/labwc/master/docs/rc.xml -O ~/.config/labwc/rc.xml
+
 else
   if [ -n "$(ls -A $DSTDIR)" ]; then
     echo "Files exist.=> $DSTDIR"
