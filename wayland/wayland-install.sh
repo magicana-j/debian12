a56358096a8cac8bef4286a7852640d9fdc06431
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-01 11:36:15 +0900

    wayland-install.sh を更新

--- wayland/wayland-install.sh
+++ wayland/wayland-install.sh
@@ -4,7 +4,7 @@ sudo apt install swaybg swayidle swaylock xdg-desktop-portal-wlr xwayland
 sudo apt install wdisplays kanshi waybar mako picom grom slurp grimshot fuzzel foot alacritty fonts-font-awesome
 sudo apt install gammastep
 sudo apt install brightnessctl clipman network-manager-gnome
-sudo apt install pipewire-audio wireplumber pipewire-media-session-
+sudo apt install pipewire-audio wireplumber pipewire-media-session-pulse
 sudo apt install pavucontrol playerctl
 sudo apt install bluetooth blueman
 sudo apt install fcitx5 fcitx5-mozc
