8174e402c7354a2f91b9a3f14d7dc4270c0e447e
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-01 19:30:22 +0900

    add-programs.sh を更新

--- add-programs.sh
+++ add-programs.sh
@@ -3,5 +3,5 @@
 sudo apt install htop vim neovim nano neofetch git ufw gufw timeshift flatpak podman fcitx5 fcitx5-mozc
 im-connfig -n fcitx5
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-LANG=C xdg-user-dirs-gtk-update
 sudo apt install gimp inkscape vlc shotcut audacity handbrake
+LANG=C xdg-user-dirs-gtk-update
