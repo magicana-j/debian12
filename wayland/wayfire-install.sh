d9187259c3099230f95da496b776e32045e33892
ruad_rofhessa <104673552+magicana-j@users.noreply.github.com>
2024-12-01 15:48:49 +0900

    Update wayfire-install.sh

--- wayland/wayfire-install.sh
+++ wayland/wayfire-install.sh
@@ -5,6 +5,7 @@ sudo apt install wayfire
 DSTDIR=~/.config/wayfire
 if [ ! -d $DSTDIR ]; then
   mkdir $DSTDIR
+  cp /usr/local/share/examples/wayfire/wayfire.ini $DSTDIR+=/
 else
   if [ -n "$(ls -A $DSTDIR)" ]; then
     echo "Files exist.=> $DSTDIR"
